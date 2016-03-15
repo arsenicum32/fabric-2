09bd941c0101ac6e318a02d43beb352d2da5d0aa
arsenicum32 <zombiehot@yandex.ru>
2016-03-15 20:59:00 +0300

    no menu in chrome

--- js/draw_panel.js
+++ js/draw_panel.js
@@ -1,21 +1,23 @@
 (function(){
-  var Rect = new fabric.Rect({
-          left: offsetX,
-          top: offsetY,
-          width: canvas.getWidth(),
-          height: 40,
-          fill: 'A9DEF9'
-        });
-  var button = new fabric.Text(' куда я попал??? ', {
-    left: offsetX+canvas.getWidth()-180,
-    top: offsetY+10,
-    fontFamily: 'Gill Sans',
-    fontSize: 20,
-    textBackgroundColor: 'black',
-    fill: '0AFFED'
-});
+  if(is.not.chrome()){
+    var Rect = new fabric.Rect({
+            left: offsetX,
+            top: offsetY,
+            width: canvas.getWidth(),
+            height: 40,
+            fill: 'A9DEF9'
+          });
+    var button = new fabric.Text(' куда я попал??? ', {
+      left: offsetX+canvas.getWidth()-180,
+      top: offsetY+10,
+      fontFamily: 'Gill Sans',
+      fontSize: 20,
+      textBackgroundColor: 'black',
+      fill: '0AFFED'
+    });
 
-  canvas.add(Rect);
-  canvas.add(button);
-  canvas.renderAll();
+    canvas.add(Rect);
+    canvas.add(button);
+    canvas.renderAll();
+  }
 })();
